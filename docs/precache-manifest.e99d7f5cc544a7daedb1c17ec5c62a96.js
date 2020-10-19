cb8695af42fa5183baccf6a56fb94ea57d688625
Christina <christinalorenzomontes@gmail.com>
2020-10-19 09:54:30 +0200

    Add run docs

---
+++ docs/precache-manifest.e99d7f5cc544a7daedb1c17ec5c62a96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94d2c1dd98a50038e2d153387bc78858",
+    "url": "/index.html"
+  },
+  {
+    "revision": "471352542987f1275ad0",
+    "url": "/static/css/main.8838ba68.chunk.css"
+  },
+  {
+    "revision": "faf2cbc118b08060a8de",
+    "url": "/static/js/2.02fad10c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.02fad10c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "471352542987f1275ad0",
+    "url": "/static/js/main.67a99c72.chunk.js"
+  },
+  {
+    "revision": "738351728e6484d30980",
+    "url": "/static/js/runtime-main.b1c034f1.js"
+  }
+]);
