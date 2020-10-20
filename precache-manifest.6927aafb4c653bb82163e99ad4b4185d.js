1ad377f3ea8134a5f56bbb7abab74b0435e0c6a3
Christina <christinalorenzomontes@gmail.com>
2020-10-20 16:10:16 +0200

    Updates

---
+++ precache-manifest.6927aafb4c653bb82163e99ad4b4185d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eba15e83fa4e94724408eb2a1fe259e",
+    "url": "/CalculadoraDeCalorias/index.html"
+  },
+  {
+    "revision": "7e9a022f6c6ad0865470",
+    "url": "/CalculadoraDeCalorias/static/css/main.0f82f06d.chunk.css"
+  },
+  {
+    "revision": "4e8cd8223da313009b52",
+    "url": "/CalculadoraDeCalorias/static/js/2.93f795de.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/CalculadoraDeCalorias/static/js/2.93f795de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e9a022f6c6ad0865470",
+    "url": "/CalculadoraDeCalorias/static/js/main.2a2353db.chunk.js"
+  },
+  {
+    "revision": "70208498fbab76022fe9",
+    "url": "/CalculadoraDeCalorias/static/js/runtime-main.688633fc.js"
+  },
+  {
+    "revision": "e725404ddedd91eaed3e7b33721821ab",
+    "url": "/CalculadoraDeCalorias/static/media/background.e725404d.jpg"
+  }
+]);
